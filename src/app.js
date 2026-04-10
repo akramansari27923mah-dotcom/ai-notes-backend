e2bad4244403d5b1a980bbd65e08e73e810463ad
Akram Ansari <akramansari27923mah@gmail.com>
2026-04-10 11:40:39 -0400

    Fix Routes

--- src/app.js
+++ src/app.js
@@ -1,42 +1,36 @@
-import express from "express";
-import authRouter from "./Routes/auth.routes.js";
-import aiRouter from "./Routes/pdf.routes.js";
-import quizRouter from "./Routes/quiz.routes.js";
-import cors from "cors";
-import cookieParser from "cookie-parser";
+import express from 'express';
+import authRouter from './Routes/auth.routes.js'
+import aiRouter from './Routes/pdf.routes.js'
+import quizRouter from './Routes/quiz.routes.js'
+import cors from 'cors';
+import cookieParser from 'cookie-parser'
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://notecraftai.vercel.app",
-];
+//MIDDLEWARE
 
 const corsOptions = {
-  origin: function(origin, callback){
-    if(!origin || allowedOrigins.includes(origin)){
-      callback(null, true)
-    }
-    else{
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-  credentials: true,
-};
+  origin: [
+    'http://localhost:5173',
+    'https://notecraftai.vercel.app'
+  ],
+  methods: ['GET', 'POST', 'DELETE', 'OPTIONS'], 
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}
 
-//MIDDLEWARE
-app.use(cors(corsOptions));
-app.options(/.*/, cors(corsOptions));
-app.use(express.json());
-app.use(cookieParser());
+app.use(cors(corsOptions))
+app.options(/.*/, cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
 
 //prefix
-app.use("/api/auth", authRouter);
-app.use("/api/ai", aiRouter);
-app.use("/api/quiz", quizRouter);
+app.use('/api/auth', authRouter)
+app.use('/api/ai', aiRouter)
+app.use('/api/quiz', quizRouter)
 
-app.get("/", (_, res) => {
-  res.send("Api is working");
-});
+app.get('/', (_, res) => {
+    res.send('Api is working')
+})
 
-export default app;
+export default app
